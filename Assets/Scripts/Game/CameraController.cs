faf8c73629605f734478a9694344e4f9cbf2db58
ripercheto <riperchetobg@gmail.com>
2021-04-24 12:24:30 +0200

    [Game] Added some boilerplate static code [Player] Added physics player controller with "better jump"

---
+++ Assets/Scripts/Game/CameraController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+partial class Game
+{
+    public static CameraController Camera => CameraController.instance;
+}
+
+public class CameraController : MonoBehaviour
+{
+    public static CameraController instance;
+
+    public Vector3 offset;
+    public float verticalAngle = 30;
+    public float length = 10;
+
+    private Quaternion rotation;
+    private Vector3 offsetPosition;
+
+    private void OnValidate()
+    {
+        rotation = Quaternion.Euler(verticalAngle, 0, 0);
+        offsetPosition = offset + rotation * (-Vector3.forward * length);
+
+        if (!Application.isPlaying)
+        {
+            transform.position = offsetPosition;
+            transform.rotation = Quaternion.LookRotation(-(offsetPosition-offset).normalized);
+        }
+    }
+
+    private void Awake()
+    {
+        instance = this;
+        OnValidate();
+    }
+
+    private void LateUpdate()
+    {
+        transform.position = Game.Player.transform.position + offsetPosition;
+    }
+}
