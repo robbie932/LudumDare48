13935e2291ccf05704751ee3b9044568c414e1e8
ripercheto <riperchetobg@gmail.com>
2021-04-24 16:44:52 +0200

    [Game] target framerate

--- Assets/Scripts/Game/Game.cs
+++ Assets/Scripts/Game/Game.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public partial class Game : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Application.targetFrameRate = 60;
     }
 }
