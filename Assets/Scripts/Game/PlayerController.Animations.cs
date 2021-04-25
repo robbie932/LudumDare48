ee3c3f7b367731b36433dabf4ee5bb825f00d020
ripercheto <riperchetobg@gmail.com>
2021-04-25 03:53:05 +0200

    particles

--- Assets/Scripts/Game/PlayerController.Animations.cs
+++ Assets/Scripts/Game/PlayerController.Animations.cs
@@ -10,7 +10,7 @@ partial class PlayerController
     public float maxRotation = 10f;
     public float forwardLean = 10f;
     public float leanSpeed = 0.6f;
-
+    public float rollChance = 0.1f;
     private float leanAmount;
     private void UpdateLeaningAnimations()
     {
@@ -20,8 +20,26 @@ partial class PlayerController
         leanAmount = Mathf.Lerp(leanAmount, a, leanSpeed);
         animator.SetFloat("Lean", leanAmount);
         animator.SetBool("OnGround", OnGround);
-        animator.SetBool("ShouldRoll", Random.value >= 0.8f);
+        animator.SetBool("ShouldRoll", Random.value >= (1 - rollChance));
         var t = Quaternion.Euler(normalizedZVel * forwardLean, 0, normalizedX * -maxRotation);
         model.localRotation = Quaternion.Lerp(model.localRotation, t, leanSpeed);
     }
 }
+#if UNITY_EDITOR
+public static class ALIGN
+{
+    // Add a menu item to create custom GameObjects.
+    // Priority 1 ensures it is grouped with the other menu items of the same kind
+    // and propagated to the hierarchy dropdown and hierarchy context menus.
+    [UnityEditor.MenuItem("GameObject/Align/Space Out", false, 10)]
+    static void SpaceOut(UnityEditor.MenuCommand menuCommand)
+    {
+        var tra = UnityEditor.Selection.transforms;
+
+        for (int i = 0; i < tra.Length; i++)
+        {
+            tra[i].position = Vector3.right * i * 20f;
+        }
+    }
+}
+#endif
