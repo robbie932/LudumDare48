8e7457568ff9f050aa06aea4b4e04495a742a095
robbie932 <r.mcdonald@live.nl>
2021-04-25 18:05:47 +0200

    #Added - Shitty object floater

--- Assets/Scripts/Game/ObjectFloater.cs
+++ Assets/Scripts/Game/ObjectFloater.cs
@@ -5,33 +5,46 @@ using UnityEngine;
 public class ObjectFloater : MonoBehaviour
 {
 
-    public float RotationSpeed = 0.0f;
+    public Vector3 RotationSpeed;
     public Vector3 StartRotation;
     public Vector3 RotationDirection; // Max 1, 1, 1
-    public bool randomStartX;
     public int RandomStartX;
     public int RandomStartY;
     public int RandomStartZ;
+     bool RandomDirFlip;
     public bool DoRandomRotate;
 
-
-
+    public Vector3 FlippedAxis;
     // Start is called before the first frame update
     void Start() {
+        if (RandomDirFlip) {
+            FlippedAxis = new Vector3(-1, -1, -1);
+        }
 
-        Vector3 AdditionalStartRotation = new Vector3(Random.Range(0, RotationSpeed * RandomStartX), Random.Range(0, RotationSpeed * RandomStartY), Random.Range(0, RotationSpeed * RandomStartZ));
+        Vector3 AdditionalStartRotation = new Vector3(Random.Range(0, 360 * RandomStartX), Random.Range(0, 360 * RandomStartY), Random.Range(0, 360 * RandomStartZ));
         this.transform.Rotate(StartRotation+ AdditionalStartRotation, Space.World);
 
+        
 
         if (DoRandomRotate) {
-            RotationDirection = new Vector3(Random.Range(0, RotationSpeed), Random.Range(0, RotationSpeed), Random.Range(0, RotationSpeed));
+            RotationDirection = Random.insideUnitCircle.normalized; // new Vector3(Random.Range(-RotationSpeed, RotationSpeed), Random.Range(-RotationSpeed, RotationSpeed), Random.Range(-RotationSpeed, RotationSpeed));
         }
+
+        if (RandomDirFlip) {
+            int FlipX = Random.Range(0, 1);
+            int FlipY = Random.Range(0, 1);
+            int FlipZ = Random.Range(0, 1);
+
+            FlippedAxis = new Vector3(-1+(2* FlipX), -1 + (2 * FlipY), -1 + (2 * FlipZ));
+        }
+
     }
 
     // Update is called once per frame
     void Update() {
-        if (RotationSpeed > 0) {
-            this.transform.Rotate(RotationDirection * RotationSpeed, Space.World);
+        if (RotationSpeed.magnitude > 0) {
+            
+            transform.Rotate( RotationSpeed , Space.Self);
         }
     }
 }
