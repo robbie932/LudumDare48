d631db6fee8034c8ecfa217ffde4cbc490df2359
robbie932 <r.mcdonald@live.nl>
2021-04-25 20:40:59 +0200

    #Added - End game trigger box code+prefab. - Trigger tar in TagManager - Set Player and Trigger to collide.

---
+++ Assets/Scripts/Game/EndGame.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndGame : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other) {
+        //Kinda redundant if-statement because all objects are static
+        if (other.gameObject.layer == LayerMask.NameToLayer("Player")) {
+            Debug.Log("Dead");
+        }
+    }
+}
