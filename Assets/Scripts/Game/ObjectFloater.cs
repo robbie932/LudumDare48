6ba6440a0b2c0df6bd9103544f8517bcc62a49ee
robbie932 <r.mcdonald@live.nl>
2021-04-26 00:12:14 +0200

    #Added - Floating prefabs - Float / rotation script - SkyDome - Skydome example prefab.

--- Assets/Scripts/Game/ObjectFloater.cs
+++ Assets/Scripts/Game/ObjectFloater.cs
@@ -4,14 +4,49 @@ using UnityEngine;
 
 public class ObjectFloater : MonoBehaviour
 {
-    public Vector3 RotationSpeed;
+    public Vector3 RotationSpeeds;
+    public bool RandomRotationSpeeds;
+    public float RotSpeedMultiplier = 1.0f;
 
-    // Update is called once per frame
+    public bool Bob;
+    public float BobSpeed;
+    public float FloatingHeight = 1.0f;
+
+    float CurrentYPos_World;
+    float SinVal;
+
+    private void Start() {
+        if (RandomRotationSpeeds) {
+            int FlipX = GetRandomBoolAsInt();print(FlipX);
+            int FlipY = GetRandomBoolAsInt();
+            int FlipZ = GetRandomBoolAsInt();
+
+            RotationSpeeds = new Vector3(
+                Random.Range(0.1f, 1f) * RotSpeedMultiplier * FlipX,
+                Random.Range(0.1f, 1f) * RotSpeedMultiplier * FlipY,
+                Random.Range(0.1f, 1f) * RotSpeedMultiplier * FlipZ) ;
+        }
+        CurrentYPos_World = this.transform.position.y;
+
+        SinVal = Random.Range(-1f, 1f); //Start Y position set to random for first round.
+        BobSpeed = BobSpeed*GetRandomBoolAsInt(); //To randomize up or down start direction
+    }
+
+    private int GetRandomBoolAsInt() {
+        return (Random.value > 0.5f) ? -1 : 1;
+    }
+
     void Update()
     {
-        if (RotationSpeed.magnitude > 0)
+        if (RotationSpeeds.magnitude > 0)
         {
-            transform.Rotate(RotationSpeed, Space.Self);
+            transform.Rotate(RotationSpeeds, Space.Self);
+        }
+
+        if (Bob) {
+            
+            transform.position = new Vector3(transform.position.x, CurrentYPos_World + (SinVal * FloatingHeight), transform.position.z);
+            SinVal = Mathf.Sin(Time.time * BobSpeed );
         }
     }
 }
